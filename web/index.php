695f43447a873b088118549542b19b2198c9c631
Malik Bensabre <malik.bensabre@gmail.com>
2019-02-10 15:33:26 +0100

    remove connection

--- web/index.php
+++ web/index.php
@@ -1,29 +1,6 @@
 <?php
 
 require('../vendor/autoload.php');
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-
-$server = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$db = substr($url["path"], 1);
-
-$conn = new mysqli($server, $username, $password, $db);
-
-if (!$conn) {
-  die("Connection failed: " . mysqli_connect_error());
-}
-
-echo "Connected successfully";
-
-// $sql = "INSERT INTO Students (name, lastname, email) VALUES ('Thom', 'Vial', 'thom.v@some.com')";
-
-// if (mysqli_query($conn, $sql)) {
-//   echo "New record created successfully";
-// } else {
-//   echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-// }
-// mysqli_close($conn);
 
 echo 'bonjour';
 
